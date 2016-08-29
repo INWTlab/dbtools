de1d020a3209b8c11f937296b7ddb8671e2d9a71
Sebastian <wahani@gmail.com>
2016-08-29 16:47:18 +0200

    some todos and doc update

--- R/helperClass.R
+++ R/helperClass.R
@@ -1,16 +1,19 @@
-#' Helper-Class: Single Query
-#'
-#' Class which represents a single query or a list of single queries. Mostly
-#' used internally.
-#'
-#' @rdname SingleQuery
-#'
-#' @details
-#' \code{SingleQuery} inherits from \code{character} and represents a single query.
-#'
-#' \code{SingleQueryList} inherits from \code{list} and represents a list of single queries. It can be constructed with a list of character values.
-#'
-#' @export
+##' SQL Query Objects
+##'
+##' Data types to represent SQL queries. It should not be necessary to use
+##' SingleQuery and SingleQueryList interactively. \code{query} is the generic
+##' user interface to generate SQL queries and is based on text parsing.
+##'
+##' @rdname queries
+##'
+##' @details
+##' \code{SingleQuery} inherits from \code{character} and represents a single
+##' query.
+##'
+##' \code{SingleQueryList} inherits from \code{list} and represents a list of
+##' single queries. It can be constructed with a list of character values.
+##'
+##' @export
 character : SingleQuery() %type% {
   assert_that(
     is.scalar(.Object),
@@ -20,11 +23,20 @@ character : SingleQuery() %type% {
   .Object
 }
 
-#' @export
-#' @rdname SingleQuery
-#'
-#' @export
+##' @export
+##' @rdname queries
+##'
+##' @export
 list : SingleQueryList() %type% {
   S3Part(.Object) <- Map(SingleQuery, .Object)
   .Object
 }
+
+##' @export
+##' @rdname queries
+Query <- function(...) {
+  ## TODO: implement this
+  templates::template(...)
+}
+
+## TODO: Some helpful assertions for 
