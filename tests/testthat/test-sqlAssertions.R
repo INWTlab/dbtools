59aed68195c080737e6ba431cc19b53038c0f356
Sebastian <wahani@gmail.com>
2017-01-27 11:32:23 +0100

    sqlAssertAlnums

--- tests/testthat/test-sqlAssertions.R
+++ tests/testthat/test-sqlAssertions.R
@@ -6,6 +6,10 @@ test_that("sqlPattern", {
     testthat::expect_error(x)
   }
 
+  expectEqual <- function(x, y) {
+    testthat::expect_equal(x, y)
+  }
+
   expectError(
     sqlPattern("1 2", "[ ]", TRUE)
   )
@@ -14,11 +18,13 @@ test_that("sqlPattern", {
     sqlPattern("12", "[ ]", FALSE)
   )
 
-  expectError(sqlChar("1"))
-  expectError(sqlChar(" "))
-  expectError(sqlChar("!"))
-  expectError(sqlNum("a1"))
-  expectError(sqlNum("a"))
+  expectError(sqlAssertChar("1"))
+  expectError(sqlAssertChar(" "))
+  expectError(sqlAssertChar("!"))
+  expectError(sqlAssertNum("a1"))
+  expectError(sqlAssertNum("a"))
+  expectError(sqlAssertAlnum("a1!"))
+  expectEqual(sqlAssertAlnum("a1"), "a1")
   
 })
 
@@ -35,7 +41,7 @@ test_that("Formats", {
   expectTrue(sqlParan(1:2) == "(1, 2)")
   expectTrue(sqlEsc(1) == "`1`")
   expectTrue(sqlEsc(1:2) == "`1`, `2`")
-  expectTrue(sqlName("a") == "`a`")
+  expectTrue(sqlName("a1") == "`a1`")
   expectError(sqlName("DROP TABLE"))
   expectTrue(sqlNames(letters[1:2]) == "`a`, `b`")
   expectTrue(sqlInChars(letters[1:2]) == "(\"a\", \"b\")")
