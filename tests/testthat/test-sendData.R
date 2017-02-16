bf535640ee67b10a6acaba57514b7b9c61fbdb4d
Jonathan Bob <jonathan.bob@inwt-statistics.de>
2017-02-16 16:12:46 +0100

    Spaltenbezeichner berücksichtigen (#25)

--- tests/testthat/test-sendData.R
+++ tests/testthat/test-sendData.R
@@ -1,19 +1,18 @@
-context("sendData-SQLite")
+testthat::context("sendData-SQLite")
 
-test_that("sendData", {
+testthat::test_that("sendData", {
 
   # prepare data
   data(mtcars, envir = environment())
-  mtcars <- mtcars %>%
-    tibble::rownames_to_column("model")
+  mtcars <- tibble::rownames_to_column(mtcars, "model")
 
   # set up connection
-  cred <- Credentials(drv = SQLite, dbname = "test.db")
+  cred <- dbtools::Credentials(drv = dbtools::SQLite, dbname = "test.db")
 
   # create table
-  sendQuery(cred, "DROP TABLE IF EXISTS `mtcars`;")
+  dbtools::sendQuery(cred, "DROP TABLE IF EXISTS `mtcars`;")
 
-  sendQuery(cred, "CREATE TABLE `mtcars` (
+  dbtools::sendQuery(cred, "CREATE TABLE `mtcars` (
     `model` TEXT PRIMARY KEY,
     `mpg` REAL,
     `cyl` REAL,
@@ -29,13 +28,13 @@ test_that("sendData", {
   )
 
   # send data to database
-  sendData(cred, mtcars)
+  dbtools::sendData(cred, mtcars)
 
   # retrieve data
-  res <- sendQuery(cred, "SELECT * FROM `mtcars`;")
+  res <- dbtools::sendQuery(cred, "SELECT * FROM `mtcars`;")
 
   # objects should be equal
-  expect_identical(res, tibble::as_data_frame(mtcars))
+  testthat::expect_identical(res, tibble::as_data_frame(mtcars))
 
   # delete database
   unlink("test.db")
@@ -45,19 +44,18 @@ test_that("sendData can operate on CredentialsList", {
 
   # prepare data
   data(mtcars, envir = environment())
-  mtcars <- mtcars %>%
-    tibble::rownames_to_column("model")
+  mtcars <- tibble::rownames_to_column(mtcars, "model")
 
   # set up connection
-  cred <- CredentialsList(
-    drv = list(SQLite, SQLite),
+  cred <- dbtools::CredentialsList(
+    drv = list(dbtools::SQLite, dbtools::SQLite),
     dbname = c("db1.db", "db2.db")
   )
 
   # create table
-  sendQuery(cred, "DROP TABLE IF EXISTS `mtcars`;")
+  dbtools::sendQuery(cred, "DROP TABLE IF EXISTS `mtcars`;")
 
-  sendQuery(cred, "CREATE TABLE `mtcars` (
+  dbtools::sendQuery(cred, "CREATE TABLE `mtcars` (
     `model` TEXT PRIMARY KEY,
     `mpg` REAL,
     `cyl` REAL,
@@ -73,21 +71,21 @@ test_that("sendData can operate on CredentialsList", {
   )
 
   # send data to database
-  sendData(cred, mtcars)
+  dbtools::sendData(cred, mtcars)
 
   # retrieve data
-  res <- sendQuery(cred, "SELECT * FROM `mtcars`;", simplify = FALSE)
+  res <- dbtools::sendQuery(cred, "SELECT * FROM `mtcars`;", simplify = FALSE)
 
   # there should be two identical instances of mtcars in the result set
-  expect_identical(res[[2]][[1]], res[[1]][[1]])
-  expect_identical(res[[1]][[1]], tibble::as_data_frame(mtcars))
+  testthat::expect_identical(res[[2]][[1]], res[[1]][[1]])
+  testthat::expect_identical(res[[1]][[1]], tibble::as_data_frame(mtcars))
 
   # delete database
   unlink(c("db1.db", "db2.db"))
 })
 
-context("sendData-RMySQL")
-test_that("sendData for RMySQL DB", {
+testthat::context("sendData-RMySQL")
+testthat::test_that("sendData for RMySQL DB", {
 
   tmp <- system(
     paste0('docker run --name test-mysql-db -p 127.0.0.1:3307:3306 ',
@@ -99,11 +97,10 @@ test_that("sendData for RMySQL DB", {
 
   # prepare data
   data(mtcars, envir = environment())
-  mtcars <- mtcars %>%
-    tibble::rownames_to_column("model")
+  mtcars <- tibble::rownames_to_column(mtcars, "model")
 
-  cred <- Credentials(
-    drv = MySQL,
+  cred <- dbtools::Credentials(
+    drv = dbtools::MySQL,
     user = "root",
     password = "root",
     dbname = "test",
@@ -112,7 +109,7 @@ test_that("sendData for RMySQL DB", {
   )
 
   # create table
-  sendQuery(cred, "CREATE TABLE `mtcars` (
+  dbtools::sendQuery(cred, "CREATE TABLE `mtcars` (
     `model` VARCHAR(20) NOT NULL,
     `mpg` DOUBLE NOT NULL,
     `cyl` DOUBLE NOT NULL,
@@ -129,29 +126,42 @@ test_that("sendData for RMySQL DB", {
   )
 
   # send data to database
-  expect_true(sendData(cred, mtcars))
+  testthat::expect_true(dbtools::sendData(cred, mtcars))
 
   # retrieve data
-  res <- sendQuery(cred, "SELECT * FROM `mtcars`;")
+  res <- dbtools::sendQuery(cred, "SELECT * FROM `mtcars`;")
 
   # objects should be equal
-  expect_identical(res, tibble::as_data_frame(mtcars) %>% dplyr::arrange(model))
+  testthat::expect_identical(
+    res,
+    dplyr::arrange(tibble::as_data_frame(mtcars), model)
+  )
 
   # duplicates in case of insert
-  expect_warning(
-    sendData(cred, mtcars, mode = "insert"),
+  testthat::expect_warning(
+    dbtools::sendData(cred, mtcars, mode = "insert"),
     regexp = "Duplicate entry"
   )
 
   # duplicates in case of replace
-  expect_true(sendData(cred, mtcars, mode = "replace"))
+  testthat::expect_true(dbtools::sendData(cred, mtcars, mode = "replace"))
 
   # truncate
-  sendData(cred, dplyr::slice(mtcars, 1:5), table = "mtcars", mode = "truncate")
+  dbtools::sendData(cred, dplyr::slice(mtcars, 1:5), table = "mtcars", mode = "truncate")
 
   # retrieve data
-  res <- sendQuery(cred, "SELECT * FROM `mtcars`;")
-  expect_identical(nrow(res), 5L)
+  res <- dbtools::sendQuery(cred, "SELECT * FROM `mtcars`;")
+  testthat::expect_identical(nrow(res), 5L)
+
+  # field order
+  testthat::expect_true(
+    dbtools::sendData(
+      cred,
+      dplyr::select(mtcars, dplyr::one_of(rev(names(mtcars)))),
+      table = "mtcars",
+      mode = "truncate"
+    )
+  )
 
   # End the temp db:
   tmp <- system(
