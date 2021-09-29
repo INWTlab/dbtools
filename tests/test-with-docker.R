2c6dceee33e2d14c815ea52f288da6eb5e8d2bd6
Sebastian Warnholz <wahani@gmail.com>
2021-09-29 14:00:02 +0200

    start and stop db in tests

--- tests/test-with-docker.R
+++ tests/test-with-docker.R
@@ -6,11 +6,20 @@ TEST <- function(topic, expr, envir = parent.frame()) {
   res <- try(
     eval(match.call()$expr, envir = new.env(parent = envir))
   )
-  if (inherits(res, "try-error")) stop(topic, ": -- F A I L E D --")
-  else cat("OK\n")
+  if (inherits(res, "try-error")) {
+    stop(topic, ": -- F A I L E D --")
+  } else {
+    cat("OK\n")
+  }
 }
 
 cat("Testing:\n")
 
-testFiles <- list.files("test-with-docker.d", full.names = TRUE)
-for (file in testFiles) source(file, new.env())
+RUN_TESTS <- function() {
+  system("bash start-db.sh")
+  on.exit(system("bash stop-db.sh"))
+  testFiles <- list.files("test-with-docker.d", full.names = TRUE)
+  for (file in testFiles) source(file, new.env())
+}
+
+RUN_TESTS()
