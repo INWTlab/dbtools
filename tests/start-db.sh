21d156bacdd3200f66954ca0697a1d54b77b0246
Sebastian Warnholz <wahani@gmail.com>
2021-09-29 20:13:48 +0200

    updating test scenarios

--- tests/start-db.sh
+++ tests/start-db.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-docker build -t mysql-test-database -f ../inst/db/mysql/Dockerfile .. \
+echo "Starting MySQL database"
+
+docker build -t mysql-test-database -f db/mysql/Dockerfile db \
     && docker run \
         --name mysql-test-database \
         -p 33001:3306 \
         -d mysql-test-database \
         --default-authentication-plugin=mysql_native_password \
         --local-infile \
-    && docker build -t mariadb-test-database -f ../inst/db/mariadb/Dockerfile .. \
+    && docker build -t mariadb-test-database -f db/mariadb/Dockerfile db \
     && docker run \
         --name mariadb-test-database \
         -p 33002:3306 \
